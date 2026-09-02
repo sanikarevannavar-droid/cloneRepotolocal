59a935d09dea109c47ed4e2dc86422e65f60d2fd
sanika <sanikarevannavar@gmail.com>
2026-09-02 14:37:18 +0530

    saving the modified file

--- colllegeinfo.py
+++ colllegeinfo.py
@@ -1,4 +1,7 @@
 college = input("Enter your college name")
 city = input("Enter your city name")
+contact = input("Enter your contact number")
+
 
 print("College basic details", college+ " " +city)
+print("College contact is", contact)
